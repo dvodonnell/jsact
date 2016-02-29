f26c19af69467b0c1e6a360e6ba4f95e3ab4676c
Daniel O'Donnell <dan@dvoc.ca>
2016-02-28 21:16:42 -0500

    more tweaks

--- src/act.js
+++ src/act.js
@@ -4,13 +4,26 @@
 
 })(window, function(){
 
-    return {
+    var Act = {
 
-        _document : document,
+        /*_document : document,
 
         setDocument : function(_document) {
             this._document = _document;
-        },
+        },*/
+
+        validDOMAttrs : [
+            'href',
+            'title',
+            'src',
+            'type',
+            'name',
+            'placeholder'
+        ],
+
+        validDOMEvents : [
+            'onsubmit'
+        ],
 
         defineElement : function(elementDef) {
             return (function(_elementDef){
@@ -22,7 +35,7 @@
 
         render : function(el, container) {
             if (container.firstChild) {
-                this._compareTree(container.firstChild, el, container);
+                Act._compareTree(container.firstChild, el, container);
             } else {
                 container.appendChild(el);
             }
@@ -32,26 +45,39 @@
             return (function(_type, _self){
                 return function(attributes, children) {
                     var elId = (attributes) ? JSON.stringify(attributes) : '';
-                    var newEl = _self._document.createElement(_type);
+                    var newEl = document.createElement(_type);
                     if (children && Array.isArray(children)) {
                         for (var i = 0; i < children.length; i++) {
                             newEl.appendChild(children[i]);
                         }
-                    } else if (children) {
-                        newEl.appendChild(_self._document.createTextNode(children));
+                    } else if (children && (children !== null && typeof children !== 'object')) {
+                        newEl.appendChild(document.createTextNode(children));
                         elId += children;
                     }
                     newEl.setAttribute("data-adapt-id", elId);
+                    if (attributes) {
+                        if (attributes.className) {
+                            newEl.setAttribute("class", attributes.className);
+                        }
+                        for (attrName in attributes) {
+                            if (Act.validDOMAttrs.indexOf(attrName) > -1) {
+                                newEl.setAttribute(attrName, attributes[attrName]);
+                            } else if (Act.validDOMEvents.indexOf(attrName) > -1) {
+                                newEl[attrName] = attributes[attrName];
+                            }
+                        }
+                    }
                     return newEl;
                 }
-            })(type, this);
+            })(type, Act);
         },
 
         _compareTree : function(originalEl, newEl, parentEl) {
             //TODO this is where the work goes
             if (originalEl.getAttribute("data-adapt-id") != newEl.getAttribute("data-adapt-id")) {
                 //TODO don't need to replace, just change attrs?
-                parentEl.replaceChild(newEl, originalEl);
+                var dupNode = newEl.cloneNode(true);
+                parentEl.replaceChild(dupNode, originalEl);
             } else if (newEl.childNodes.length === 0) {
                 while (originalEl.firstChild) {
                     originalEl.removeChild(originalEl.firstChild);
@@ -61,12 +87,12 @@
                     nLen = newEl.childNodes.length;
                 if (nLen === oLen) {
                     for (var i = 0; i < nLen; i++) {
-                        this._compareTree(originalEl.childNodes[i], newEl.childNodes[i], originalEl);
+                        Act._compareTree(originalEl.childNodes[i], newEl.childNodes[i], originalEl);
                     }
                 } else if (nLen > oLen) {
                     for (var i = 0; i < nLen; i++) {
                         if (i < oLen) {
-                            this._compareTree(originalEl.childNodes[i], newEl.childNodes[i], originalEl);
+                            Act._compareTree(originalEl.childNodes[i], newEl.childNodes[i], originalEl);
                         } else {
                             originalEl.appendChild(newEl.childNodes[i]);
                         }
@@ -74,7 +100,7 @@
                 } else if (nLen < oLen) {
                     for (var i = 0; i < oLen; i++) {
                         if (i < nLen) {
-                            this._compareTree(originalEl.childNodes[i], newEl.childNodes[i], originalEl);
+                            Act._compareTree(originalEl.childNodes[i], newEl.childNodes[i], originalEl);
                         } else {
                             originalEl.removeChild(originalEl.childNodes[i]);
                         }
@@ -98,5 +124,7 @@
         }
 
     };
+
+    return Act;
 
 });
